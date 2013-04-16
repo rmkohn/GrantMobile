c76bc274f581c158cb27b9b78ce882c8aa1c98ac
Ryan Scott <rscot138@gmail.com>
2013-04-16 17:58:03 -0500

    Cleaning up some warnings

--- GrantMobile/src/com/example/grantmobile/SubmitDialog.java
+++ GrantMobile/src/com/example/grantmobile/SubmitDialog.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 //import android.app.DialogFragment;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
