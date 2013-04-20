bdb265573a9afaa688771b4fb95be6fa9f38a3e1
rmkohn <rm_kohn@yahoo.com>
2013-04-20 18:21:43 -0500

    Grant Access Service installed

---
+++ GrantMobile/src/com/example/grantmobile/GrantService.java
@@ -0,0 +1,132 @@
+package com.example.grantmobile;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+
+public class GrantService extends IntentService {
+	
+	private static final String TAG_SUCCESS = "success";  // "true" is good
+	private static final String TAG_MESSAGE = "message";
+
+	private static final String TAG_HOURS = "hours"; 
+	private static final String TAG_GRANT = "grant";
+	private static final String TAG_NON_GRANT = "non-grant";
+	private static final String TAG_LEAVE = "leave";
+	
+	private static final String TAG_MONTH = "month";
+	private static final String TAG_YEAR = "year";
+	
+	private static final String TAG_EMPLOYEE = "employee";
+	private static final String TAG_FIRST_NAME = "firstname";
+	private static final String TAG_LAST_NAME = "lastname";
+	private static final String TAG_EMPLOYEE_ID = "id";
+	
+	private static final String TAG_GRANT_ID = "ID";
+	private static final String TAG_STATE_CATALOG_NUM = "stateCatalogNum";
+	private static final String TAG_GRANT_NUMBER = "grantNumber";
+	private static final String TAG_GRANT_TITLE = "grantTitle";
+	
+	private static String requestURL = "http://mid-state.net/mobileclass2/android";
+	private static final String TAG = "grantservice";
+	public static String requestId;
+	private int result;
+	
+	JSONParser jParser;
+	ArrayList<String> grantHours; // grant hour array 
+	ArrayList<String> nonGrantHours; // non-grant hour array
+	ArrayList<String> leaveHours;  // leave hour array
+	HashMap<String,String> map;
+	JSONObject json = null; // entire json object
+	
+	public GrantService() {
+		super("GrantService");
+		jParser = new JSONParser();
+		grantHours = new ArrayList<String>(); // grant hour array 
+		nonGrantHours = new ArrayList<String>(); // non-grant hour array
+		leaveHours = new ArrayList<String>();  // leave hour array
+		map = new HashMap<String,String>();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		JSONArray jsa = null; // json array
+		JSONObject jso, jsom, jsoh = null; // misc., message & hours object
+	    Bundle extras = intent.getExtras();
+    	Message msg = Message.obtain();
+	    if (extras != null) {
+	    	Messenger messenger = (Messenger) extras.get("MESSENGER");
+	    	requestId = extras.getString(DetailViewActivity.TAG_REQUEST_ID);
+			Log.d(TAG+"_REQ_ID", "requestId = " + requestId);
+	    	List<NameValuePair> params = new ArrayList<NameValuePair>();
+	    	params.add(new BasicNameValuePair("q","email"));
+	    	params.add(new BasicNameValuePair("id",requestId)); // this will come from intent
+	    	json = JSONParser.makeHttpRequest(requestURL, "GET", params);
+			Log.d(TAG+"_GET_RET", json.toString());
+	    	try {
+	    		String s = json.getString(TAG_SUCCESS);
+	    		if (s.equals("true")) {
+	    			Log.d(TAG+"_processing...", "success = " + s);
+	    			Bundle returnData = new Bundle(); // going to return a "Bundle"
+	    			jsom = json.getJSONObject(TAG_MESSAGE);
+	    			jsoh = jsom.getJSONObject(TAG_HOURS);
+	    			jsa = jsoh.getJSONArray(TAG_GRANT);
+	    			for (int i = 0; i < jsa.length(); i++) {
+	    				grantHours.add((Integer.valueOf(jsa.getInt(i))).toString());  // these are the grant hours for each day of month
+	    			}
+	    			returnData.putStringArrayList(TAG_GRANT,grantHours);
+	    			jsa = jsoh.getJSONArray(TAG_NON_GRANT);
+	    			for (int i = 0; i < jsa.length(); i++){
+	    				nonGrantHours.add((Integer.valueOf(jsa.getInt(i))).toString());  // these are the non-grant hours for each day of month
+	    			}
+	    			returnData.putStringArrayList(TAG_NON_GRANT,nonGrantHours);
+	    			jsa = jsoh.getJSONArray(TAG_LEAVE);
+	    			for (int i = 0; i < jsa.length(); i++){
+	    				leaveHours.add((Integer.valueOf(jsa.getInt(i))).toString());  // these are the leave hours for each day of month
+	    			}
+	    			returnData.putStringArrayList(TAG_LEAVE,leaveHours);
+	    			returnData.putString(TAG_MONTH,(Integer.valueOf(jsom.getInt(TAG_MONTH))).toString());
+	    			returnData.putString(TAG_YEAR,(Integer.valueOf(jsom.getInt(TAG_YEAR))).toString());
+	    			jso = jsom.getJSONObject(TAG_EMPLOYEE);
+	    			returnData.putString(TAG_FIRST_NAME, jso.getString(TAG_FIRST_NAME));
+	    			returnData.putString(TAG_LAST_NAME, jso.getString(TAG_LAST_NAME));
+	    			Log.d(TAG+"_GET_REQ", "last name = " + jso.getString(TAG_LAST_NAME));
+	    			returnData.putString(TAG_EMPLOYEE_ID, jso.getString(TAG_EMPLOYEE_ID));
+	    			jso = jsom.getJSONObject(TAG_GRANT);	
+	    			returnData.putString(TAG_GRANT_ID, jso.getString(TAG_GRANT_ID));
+	    			returnData.putString(TAG_GRANT_NUMBER, jso.getString(TAG_GRANT_NUMBER));
+	    			returnData.putString(TAG_GRANT_TITLE, jso.getString(TAG_GRANT_TITLE));
+	    			returnData.putString(TAG_STATE_CATALOG_NUM, jso.getString(TAG_STATE_CATALOG_NUM));	
+	    			// now return via extras the grant and hour data...	
+			    	msg.setData(returnData);
+			    	result = Activity.RESULT_OK;
+	    		} else {
+	    			result = Activity.RESULT_CANCELED;
+	    		}
+			    try {
+		    		msg.arg1 = result;
+			   		messenger.send(msg);
+			    } catch (android.os.RemoteException e1) {
+			    	Log.w(getClass().getName(), "Exception sending message", e1);
+			    }
+	    	} catch (JSONException e) {
+	    		e.printStackTrace();
+	    	}
+	    }
+	}
+}
