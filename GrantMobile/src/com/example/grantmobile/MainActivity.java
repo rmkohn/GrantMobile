7cfc8a3c5c469c13304404e6251b9b9bf000f0e2
wartman4404 <wartman4404@my.mstc.edu>
2013-05-03 00:05:01 -0500

    fix further mainactivity crashes

--- GrantMobile/src/com/example/grantmobile/MainActivity.java
+++ GrantMobile/src/com/example/grantmobile/MainActivity.java
@@ -95,7 +95,12 @@ public class MainActivity extends Activity
 		
 		String id = data.getQueryParameter("ID");
 		
-		return Integer.parseInt(id);
+		if (id != null) {
+			try {
+				return Integer.parseInt(id);
+			} catch (NumberFormatException e) { }
+		}
+		return -1;
 	}//END GETWORKMONTHID
 		
 		
